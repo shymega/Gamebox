52d6f2a5dba696629d889775b6d625f574c91456
Dom Rodriguez <shymega@shymega.org.uk>
2023-11-21 22:17:06 +0000

    chore(REUSE): Add remaining SPDX headers

--- default.nix
+++ default.nix
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2023 The Steam-ToyBox Developers
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 (import
   (
     let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
--- steam-toybox.nix
+++ steam-toybox.nix
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2023 The Steam-ToyBox Developers
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 { lib
 , pkgs ? import <nixpkgs>
 , rustPlatform
