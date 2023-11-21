cee2575089f4ecea4cb44368117436fc9e240ff1
Dom Rodriguez <shymega@shymega.org.uk>
2023-11-21 22:10:53 +0000

    feat(Nix): Add a Nix package and Flake

--- flake.nix
+++ flake.nix
@@ -1,35 +1,41 @@
-# SPDX-FileCopyrightText: 2023 The Steam-ToyBox Developers
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
-
-  outputs = { self, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
       let
-        pkgs = (import nixpkgs) {
-          inherit system;
-        };
+        pkgs = nixpkgs.outputs.legacyPackages.${system};
+      in
+      {
+        packages.steam-toybox = pkgs.callPackage ./steam-toybox.nix { };
+        packages.default = self.outputs.packages.${system}.steam-toybox;
 
-        naersk' = pkgs.callPackage naersk {};
-
-      in rec {
-        # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
-          src = ./.;
-          nativeBuildInputs = with pkgs; [ pkg-config cmake ] ;
-          buildInputs = with pkgs; [ fontconfig ];
-        };
-
-        # For `nix develop`:
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo ];
-        };
-      }
-    );
+        devShells.default = self.packages.${system}.default.overrideAttrs (super: {
+          nativeBuildInputs = with pkgs;
+            super.nativeBuildInputs
+            ++ [
+              cargo-edit
+              clippy
+              rustfmt
+            ];
+          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+        });
+      })
+    // {
+      overlays.default = final: prev: {
+        inherit (self.packages.${final.system}) steam-toybox;
+      };
+    };
 }
